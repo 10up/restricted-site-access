dfa8eecd2dc0ddd4f383f7e4b71ba4972deabc87
Taylor Lovett <tlovett88@gmail.com>
2017-03-13 17:14:02 -0400

    Add watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,20 @@ module.exports = function(grunt) {
 					'assets/js/restricted-site-access.min.js': ['assets/js/src/restricted-site-access.js'],
 				}
 			}
+		},
+
+		watch: {
+			files: [
+				'assets/js/src/*'
+			],
+			tasks: ['uglify:js']
 		}
 
 	});
-	
+
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
 	grunt.registerTask( 'i18n', ['makepot'] );
 	grunt.registerTask( 'default', ['uglify:js'] );
