af7dc3bcbb97f4867012388a3b6d4900bae3ec5a
Ivan Kristianto <ivan.kristianto@gmail.com>
2017-05-17 12:33:03 +0700

    show warning message if any page cache plugin activated

--- restricted_site_access.php
+++ restricted_site_access.php
@@ -289,6 +289,8 @@ class Restricted_Site_Access {
 		if ( RSA_IS_NETWORK  && is_network_admin() ) {
 			add_action( 'load-settings.php', array( __CLASS__, 'load_network_settings_page' ) );
 		}
+
+		add_action( 'admin_notices', array( __CLASS__, 'page_cache_notice' ) );
 	}
 
 	/**
@@ -509,6 +511,20 @@ class Restricted_Site_Access {
 
 		if ( isset( $message ) ) {
 			echo '<div class="error"><p><strong>' . $message . '</strong></p></div>';
+		}
+	}
+
+	/**
+	 * Check if the page caching is on, and notify the admin
+	 */
+	public static function page_cache_notice() {
+		//If WP_CACHE is on we show notification
+		if ( true === WP_CACHE ) {
+			$notif_message = __( 'You have page caching activated. Restricted Site Access plugin may not working as expected for cached page.', 'restricted-site-access' );
+		}
+
+		if ( isset( $notif_message ) ) {
+			echo '<div class="error"><p><strong>' . $notif_message . '</strong></p></div>';
 		}
 	}
 
