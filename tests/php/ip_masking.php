b0e0cd46c53b2911d37582f31425e4634966b84d
Adam Silverstein <adam@10up.com>
2018-02-13 23:31:25 -0500

    add and refine tests

--- tests/php/ip_masking.php
+++ tests/php/ip_masking.php
@@ -1,16 +1,9 @@
 <?php
 
 class IpMaskingTests extends \PHPUnit\Framework\TestCase {
-	public function setUp() {
-		\WP_Mock::setUp();
-	}
-
-	public function tearDown() {
-		\WP_Mock::tearDown();
-	}
 
 	/**
-	 * Test that ip adcdresses pass or fail as expected when compared against an ip with a mask.
+	 * Test that ip addresses pass or fail as expected when compared against an ip with a mask.
 	 *
 	 * @dataProvider ip_data_provider
 	 *
@@ -20,19 +13,28 @@ class IpMaskingTests extends \PHPUnit\Framework\TestCase {
 	 */
 	public function testAdd( $ip_to_test, $ip_plus_mask, $expected )
 	{
-		$this->assertEquals( $expected, Restricted_Site_Access::ip_in_mask( $ip_to_test, $ip_plus_mask ) );
+		$this->assertEquals( $expected, ip_in_range( $ip_to_test, $ip_plus_mask ) );
 	}
 
 	public function ip_data_provider()
 	{
 		return [
-			[ '192.168.0.1', '192.168.0.1', true ],
-			[ '192.168.0.1', '192.168.0.1', true ],
-			[ '192.168.0.1', '192.168.0.1', true ],
-			[ '192.168.1.1', '102.1.5.2/24', true ],
+			[ '192.168.0.1', '192.168.0.1',    true ],
+			[ '192.168.0.1', '192.168.0.1',    true ],
+			[ '192.168.0.1', '192.168.0.1',    true ],
+			[ '192.168.1.1', '192.1.5.2/24',   false ],
+			[ '192.168.1.1', '192.168.5.2/24', false ],
+			[ '192.168.1.1', '192.168.5.2/16', true ],
 			[ '192.168.1.1', '192.168.1.0/24', true ],
-			[ '192.168.1.1', '192.168.1.1', true ]
-
+			[ '192.168.1.1', '192.168.1.2',    false ],
+			[ '192.168.1.1', '192.168.1.2/24', true ],
+			[ '192.168.0.1', '192.169.2.2',    false ],
+			[ '192.168.0.1', '192.169.2.2/24', false ],
+			[ '192.169.0.1', '192.169.2.2/24', false ],
+			[ '192.169.2.1', '192.169.2.2/24', true ],
+			[ '192.168.0.1', '192.169.2.2/16', false ],
+			[ '192.169.0.1', '192.169.2.2/16', true ],
+			[ '192.168.0.1', '192.169.2.2/8',  true ]
 		];
 	}
 }
