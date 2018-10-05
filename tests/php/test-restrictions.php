13c187d8c2d6bb49722252a8ef6069869b5ebea3
Pete Nelson <pete@petenelson.com>
2018-10-05 15:34:43 -0500

    added test_get_options

--- tests/php/test-restrictions.php
+++ tests/php/test-restrictions.php
@@ -2,8 +2,21 @@
 
 class Restricted_Site_Access_Test_Restrictions extends WP_UnitTestCase {
 
-	public function test_its_working() {
-		$this->assertTrue( true );
+	public function test_is_restricted() {
+
+		if ( is_multisite() ) {
+			// TODO 
+		} else {
+
+			// $this->assertSame( 'default', $rsa::)
+
+
+
+
+		}
+
+
+
 	}
 
 }
--- tests/php/test-settings.php
+++ tests/php/test-settings.php
@@ -51,4 +51,22 @@ class Restricted_Site_Access_Test_Settings extends WP_UnitTestCase {
 			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
 		}
 	}
+
+	public function test_get_options() {
+
+		$rsa = Restricted_Site_Access::get_instance();
+		$options = $rsa::get_options( is_multisite() );
+
+		$defaults = array(
+			'approach'      =>  1,
+			'message'       => _x( 'Access to this site is restricted.', 'default restriction message', 'restricted-site-access' ),
+			'redirect_url'  => '',
+			'redirect_path'	=> 0,
+			'head_code'     => 302,
+			'page'          => 0,
+			'allowed'       => array(),
+		);
+
+		$this->assertSame( $defaults, $options );
+	}
 }
