4b44929896f5f193a3b6a43e470f5c2d36118abe
Pete Nelson <pete@petenelson.com>
2018-10-05 14:09:13 -0500

    refactored tests for activation and deactivation

--- tests/php/test-settings.php
+++ tests/php/test-settings.php
@@ -2,36 +2,53 @@
 
 class Restricted_Site_Access_Test_Settings extends WP_UnitTestCase {
 
-	public function test_activation() {
+	public function test_activation_deactivation() {
 
-		$blog_public = get_option( 'blog_public' );
+		if ( is_multisite() ) {
+			// Public by default.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
 
-		// Public by default.
-		$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
+			// Activate for the current site.
+			do_action( 'activate_' . RSA_TEST_PLUGIN_BASENAME );
 
-		do_action( 'activate_' . RSA_TEST_PLUGIN_BASENAME );
+			// Current site should be restricted.
+			$this->assertSame( 2, absint( get_option( 'blog_public' ) ) );
 
-		// Now it should be restricted.
-		$this->assertSame( 2, absint( get_option( 'blog_public' ) ) );
+			// Deactivate the plugin.
+			do_action( 'deactivate_' . RSA_TEST_PLUGIN_BASENAME );
 
-		// Reset it.
-		update_option( 'blog_public', '1' );
+			// Blog should now be public.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
 
-		// TODO needs to be updated to test multi-site.
+			// Now network-activate the plugin.
+			do_action( 'activate_' . RSA_TEST_PLUGIN_BASENAME, true);
+
+			// Current site should still be public.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
+
+			// Set the current site to restricted.
+			update_option( 'blog_public', '2' );
+
+			// Now network deactivate the plugin.
+			do_action( 'deactivate_' . RSA_TEST_PLUGIN_BASENAME, true );
+
+			// Site should now be public.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
+		} else {
+
+			// Public by default.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
+
+			do_action( 'activate_' . RSA_TEST_PLUGIN_BASENAME );
+
+			// Now it should be restricted.
+			$this->assertSame( 2, absint( get_option( 'blog_public' ) ) );
+
+			// Deactivate the plugin.
+			do_action( 'deactivate_' . RSA_TEST_PLUGIN_BASENAME );
+
+			// Site should now be public.
+			$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
+		}
 	}
-
-	public function test_deactivation() {
-
-		// Set the site to restricted
-		update_option( 'blog_public', '2' );
-
-
-		do_action( 'deactivate_' . RSA_TEST_PLUGIN_BASENAME );
-
-		// Now it should be un-restricted.
-		$this->assertSame( 1, absint( get_option( 'blog_public' ) ) );
-
-		// TODO needs to be updated to test multi-site.
-	}
-
 }
