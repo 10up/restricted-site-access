73f36f3840633245cf14db15addf8ae64632fe1a
Pete Nelson <pete@petenelson.com>
2018-10-08 18:55:27 -0500

    refactored whitelisted IP tests

--- tests/php/test-restrictions.php
+++ tests/php/test-restrictions.php
@@ -49,39 +49,39 @@ class Restricted_Site_Access_Test_Restrictions extends WP_UnitTestCase {
 
 	public function test_restrict_access_restricted_whitelist() {
 
-		if ( ! defined( 'RSA_IP_WHITELIST' ) ) {
-			$this->markTestSkipped( 'RSA_IP_WHITELIST not defined' );
-			return;
-		}
-
 		$rsa = Restricted_Site_Access::get_instance();
 
 		// Set site to to restricted.
 		update_option( 'blog_public', 2 );
 
-		// Verify that the RSA_IP_WHITELIST variable is set.
-		// export RSA_IP_WHITELIST="192.168.1.50|192.168.1.51" .
-		$ips = $rsa::get_config_ips();
-
-		$this->assertCount( 2, $ips );
-		$this->assertSame( '127.0.0.1', $ips[0] );
-		$this->assertSame( '192.168.1.51', $ips[1] );
-
 		// Go to the home page.
 		$this->go_to( home_url( '/' ) );
 		$wp = $GLOBALS['wp'];
 
-		$results = $rsa::restrict_access_check( $wp );
-
 		// Check the default results.
 		$url = add_query_arg( 'redirect_to', rawurlencode( '/' ), home_url( 'wp-login.php' ) );
+
+		$results = $rsa::restrict_access_check( $wp );
 
 		$this->assertNotEmpty( $results );
 		$this->assertSame( 302, $results['code'] );
 		$this->assertSame( $url, $results['url'] );
 
+		// Add some allowed IPs.
+		$options = $rsa::get_options( is_multisite() );
+		$options['allowed'][] = '127.0.0.1';
+
+		if ( is_multisite() ) {
+			update_site_option( 'rsa_options', $options );
+		} else {
+			update_option( 'rsa_options', $options );
+		}
+
+		$options = $rsa::get_options( is_multisite() );
+		$this->assertContains( '127.0.0.1', $options['allowed'] );
+
 		// Now set the client IP to one of the whitelisted IPs.
-		$_SERVER['HTTP_CLIENT_IP'] = $ips[0];
+		$_SERVER['HTTP_CLIENT_IP'] = '127.0.0.1';
 
 		// Go to the home page.
 		$this->go_to( home_url( '/' ) );
@@ -92,5 +92,15 @@ class Restricted_Site_Access_Test_Restrictions extends WP_UnitTestCase {
 		$this->assertEmpty( $results );
 
 		unset( $_SERVER['HTTP_CLIENT_IP'] );
+
+		$options['allowed'][] = [];
+
+		if ( is_multisite() ) {
+			update_site_option( 'rsa_options', $options );
+		} else {
+			update_option( 'rsa_options', $options );
+		}
+
 	}
+
 }
