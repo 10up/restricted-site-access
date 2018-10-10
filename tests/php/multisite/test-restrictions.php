85cb09e7503d49c552468c52619876116f48886e
Pete Nelson <pete@petenelson.com>
2018-10-10 12:30:00 -0500

    added test_multiesite_restrict_access_restricted_default

--- tests/php/multisite/test-restrictions.php
+++ tests/php/multisite/test-restrictions.php
@@ -22,4 +22,34 @@ class Restricted_Site_Access_Test_Multisite_Restrictions extends WP_UnitTestCase
 		// The network public setting should allow access.
 		$this->assertEmpty( $rsa::restrict_access_check( $wp ) );
 	}
+
+	public function test_multiesite_restrict_access_restricted_default() {
+
+		$rsa = Restricted_Site_Access::get_instance();
+
+		$this->assertTrue( RSA_IS_NETWORK );
+
+		// Set network to enforced and restricted.
+		update_site_option( 'rsa_mode', 'enforce' );
+		update_site_option( 'blog_public', 2 );
+
+		// Set the individual site to not restricted.
+		update_option( 'blog_public', 1 );
+
+		// Go to the home page.
+		$this->go_to( home_url( '/' ) );
+		$wp = $GLOBALS['wp'];
+
+		// The network restricted setting should override the
+		// site's public setting.
+		$results = $rsa::restrict_access_check( $wp );
+
+		// Check the default results.
+		$url = add_query_arg( 'redirect_to', rawurlencode( '/' ), home_url( 'wp-login.php' ) );
+
+		$this->assertNotEmpty( $results );
+		$this->assertSame( 302, $results['code'] );
+		$this->assertSame( $url, $results['url'] );
+
+	}
 }
