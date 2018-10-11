37062a7ff4778c1c307517aff6d6bef9dcad9a80
Pete Nelson <pete@petenelson.com>
2018-10-11 09:57:23 -0500

    added test_privacy_link_filters

--- tests/php/test-admin.php
+++ tests/php/test-admin.php
@@ -2,14 +2,23 @@
 
 class Restricted_Site_Access_Test_Admin extends WP_UnitTestCase {
 
+	private $_init_done = false;
+
+	public function run_admin_init() {
+		if ( ! $this->_init_done ) {
+			$rsa = Restricted_Site_Access::get_instance();
+			$rsa::admin_init();
+			$this->_init_done = true;
+		}
+	}
+
 	public function test_admin_init() {
 		global $wp_settings_sections, $wp_settings_fields;
 
 		$settings_page = 'reading';
 
 		$rsa = Restricted_Site_Access::get_instance();
-
-		$rsa::admin_init();
+		$this->run_admin_init();
 
 		$this->assertSame( 10, has_filter( 'privacy_on_link_text', [ 'Restricted_Site_Access', 'privacy_on_link_text' ] ) );
 		$this->assertSame( 10, has_filter( 'privacy_on_link_title', [ 'Restricted_Site_Access', 'privacy_on_link_title' ] ) );
@@ -34,5 +43,22 @@ class Restricted_Site_Access_Test_Admin extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'head_code', $wp_settings_fields[ $settings_page ]['restricted-site-access'] );
 		$this->assertArrayHasKey( 'page', $wp_settings_fields[ $settings_page ]['restricted-site-access'] );
 		$this->assertArrayHasKey( 'allowed', $wp_settings_fields[ $settings_page ]['restricted-site-access'] );
+	}
+
+	public function test_privacy_link_filters() {
+
+		$rsa = Restricted_Site_Access::get_instance();
+
+		// Set blog to public.
+		update_option( 'blog_public', 1 );
+
+		$this->assertSame( 'test-link-text', $rsa::privacy_on_link_text( 'test-link-text' ) );
+		$this->assertSame( 'test-link-title', $rsa::privacy_on_link_title( 'test-link-title' ) );
+
+		// Set blog to restricted.
+		update_option( 'blog_public', 2 );
+
+		$this->assertSame( 'Public access to this site has been restricted.', $rsa::privacy_on_link_text( 'test-link-text' ) );
+		$this->assertSame( 'Restricted Site Access plug-in is blocking public access to this site.', $rsa::privacy_on_link_title( 'test-link-title' ) );
 	}
 }
