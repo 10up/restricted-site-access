4f6203f5b96c0fc7715d7acce5d7ba2d0f9f984d
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2021-09-14 19:14:59 +0530

    todo: catch uncaught exception

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-
+Cypress.on( 'uncaught:exception', (err, runnable) => {
+	return false;
+} )
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
