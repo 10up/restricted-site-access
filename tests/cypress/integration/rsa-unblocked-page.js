7c441a76e69dd8b5f8267ce0597889f8762a1793
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2021-12-08 16:57:26 +0530

    fix: rewrite test without /accessible-page

--- tests/cypress/integration/rsa-unblocked-page.js
+++ tests/cypress/integration/rsa-unblocked-page.js
@@ -26,14 +26,11 @@ describe( 'Show a page to restricted users', () => {
 	} );
 
 	it( 'Unrestricted user should be able to access the site', () => {
-		cy.visitAdminPage( 'options-permalink.php' );
-		cy.visitAdminPage( 'edit.php?post_type=page' );
-		cy.wait( 8000 )
-		cy.visit( `${ Cypress.config().baseUrl }accessible-page`, {
+		cy.visit( `${ Cypress.config().baseUrl }`, {
 			headers: {
 				'X-Forwarded': '193.168.20.30',
 			}
 		} );
-		cy.url().should( 'include', `${ Cypress.config().baseUrl }accessible-page` )
+		cy.contains( 'Hello world!' )
 	} );
 } );
