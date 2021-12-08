b427580bf819fec36c09ad6d64d56ae91f2b7b8d
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2021-12-08 16:43:18 +0530

    fix: cy.wait() for video recording to end later

--- tests/cypress/integration/rsa-unblocked-page.js
+++ tests/cypress/integration/rsa-unblocked-page.js
@@ -28,6 +28,7 @@ describe( 'Show a page to restricted users', () => {
 	it( 'Unrestricted user should be able to access the site', () => {
 		cy.visitAdminPage( 'options-permalink.php' );
 		cy.visitAdminPage( 'edit.php?post_type=page' );
+		cy.wait( 8000 )
 		cy.visit( `${ Cypress.config().baseUrl }accessible-page`, {
 			headers: {
 				'X-Forwarded': '193.168.20.30',
