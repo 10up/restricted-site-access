907013b7ee46a984e8c15dddb669989e47fef567
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-02-02 17:31:26 +0530

    fic: config Grunt, eslint and scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@
  * Internal dependencies
  */
 
- module.exports = {
+module.exports = {
 	extends: 'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
+	globals: {
+		window: 'readonly',
+		$: 'readonly',
+		jQuery: 'readonly',
+		rsaAdmin: 'readonly',
+		rsaSettings: 'readonly',
+		ajaxurl: 'readonly',
+	},
 };
