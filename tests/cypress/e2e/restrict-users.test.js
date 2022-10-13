7571a5e4d1f3d0ac203eef4826b16a18402cdac8
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-10-13 14:18:44 +0530

    refactor restrict user test

--- tests/cypress/e2e/restrict-users.test.js
+++ tests/cypress/e2e/restrict-users.test.js
@@ -2,15 +2,7 @@ describe( 'Handle restricted visitors - Show simple message', () => {
 	before( () => {
 		cy.wpCli( `network meta set 1 blog_public 2` );
 		cy.wpCli( `network meta set 1 rsa_mode enforce` );
-		cy.wpCli( `network meta set 1 rsa_options '{"approach":3,"message":"Access to this site is restricted.","redirect_path":0,"head_code":302,"redirect_url":"","page":0,"allowed":[],"comment":[""]}' --format=json` );
-		cy.login();
-		cy.visitAdminPage( 'network/settings.php' );
-	} );
-
-	it( 'Admin setup', () => {
-		cy.addIp( '172.13.24.5', 'Unrestricted user' );
-		cy.saveSettings();
-		cy.logout();
+		cy.wpCli( `network meta set 1 rsa_options '{"approach":3,"message":"Access to this site is restricted.","redirect_path":0,"head_code":302,"redirect_url":"","page":0,"allowed":["172.13.24.5"],"comment":[""]}' --format=json` );
 	} );
 
 	it( 'Verify - Show simple message', () => {
@@ -23,21 +15,8 @@ describe( 'Handle restricted visitors - Show simple message', () => {
 } );
 
 describe( 'Handle restricted visitors - Redirect to web address', () => {
-	before( () => {
-		cy.wpCli( `network meta set 1 blog_public 2` );
-		cy.wpCli( `network meta set 1 rsa_mode enforce` );
-		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":301,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":[],"comment":[]}' --format=json` );
-		cy.login();
-		cy.visitAdminPage( 'network/settings.php' );
-	} );
-
-	it( 'Admin setup', () => {
-		cy.addIp( '172.13.24.5', 'Unrestricted user' );
-		cy.saveSettings();
-		cy.logout();
-	} );
-
 	it( 'Verify - Redirection with status code 301', () => {
+		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":301,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":["172.13.24.5"],"comment":[]}' --format=json` );
 		cy.request( {
 			url: 'sample-page',
 			followRedirect: false,
@@ -48,7 +27,7 @@ describe( 'Handle restricted visitors - Redirect to web address', () => {
 	} );
 
 	it( 'Verify - Redirection with status code 302', () => {
-		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":302,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":[],"comment":[]}' --format=json` );
+		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":302,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":["172.13.24.5"],"comment":[]}' --format=json` );
 		cy.request( {
 			url: 'sample-page',
 			followRedirect: false,
@@ -59,7 +38,7 @@ describe( 'Handle restricted visitors - Redirect to web address', () => {
 	} );
 
 	it( 'Verify - Redirection with status code 307', () => {
-		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":307,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":[],"comment":[]}' --format=json` );
+		cy.wpCli( `network meta set 1 rsa_options '{"approach":2,"message":"Access to this site is restricted.","redirect_path":0,"head_code":307,"redirect_url":"http:\/\/localhost:8889\/page-to-redirect\/","page":0,"allowed":["172.13.24.5"],"comment":[]}' --format=json` );
 		cy.request( {
 			url: 'sample-page',
 			followRedirect: false,
