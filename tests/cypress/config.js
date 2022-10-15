712c0f4d68b177ad623de27681c7ad9ce39b3dfe
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-10-15 08:57:43 +0530

    remove old cypress tests

--- tests/cypress/config.js
+++ tests/cypress/config.js
@@ -12,17 +12,6 @@ module.exports = defineConfig({
       return setBaseUrl(on, config);
     },
     specPattern: [
-      // "tests/cypress/e2e/rsa-plugin-activation-deactivation.test.js",
-      // "tests/cypress/e2e/rsa-settings-spec.test.js",
-      // "tests/cypress/e2e/rsa-admin-settings-interaction.test.js",
-      // "tests/cypress/e2e/rsa-add-remove-ip-addresses.test.js",
-      // "tests/cypress/e2e/rsa-redirection-settings.test.js",
-      // "tests/cypress/e2e/rsa-simple-message-settings.test.js",
-      // "tests/cypress/e2e/rsa-redirect-to-login.test.js",
-      // "tests/cypress/e2e/rsa-redirect-to-web-address.test.js",
-      // "tests/cypress/e2e/rsa-unrestrict-ip.test.js",
-      // "tests/cypress/e2e/rsa-unblocked-page.test.js",
-      // "tests/cypress/e2e/rsa-multisite-newtork-settings.test.js"
       "tests/cypress/e2e/activation-deactivation.test.js",
       "tests/cypress/e2e/add-valid-addresses.test.js",
       "tests/cypress/e2e/add-invalid-addresses.test.js",
