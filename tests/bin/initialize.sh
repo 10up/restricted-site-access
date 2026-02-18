95d580dd07c474cd4ff05dfca46a5508c7bed48f
Darin Kotter <darin.kotter@gmail.com>
2026-02-18 11:53:18 -0700

    Modify wp-env config. Remove unneeded init step. Fix test command

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 wp-env run tests-wordpress chmod -c ugo+w /var/www/html
 
-wp-env run tests-cli wp core multisite-convert --title='RSA Multisite'
 wp-env run tests-cli wp post create --post_type=page --post_title='One' --post_status='publish'
 wp-env run tests-cli wp post create --post_type=page --post_title='Two' --post_status='publish'
 wp-env run tests-cli wp plugin activate rsa-seeder --network
