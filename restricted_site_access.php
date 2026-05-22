a1e7c93a0648947f9522bde7a9f4998ba20cd107
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2026-05-22 12:01:09 +1000

    Apply suggestions from code review
    Co-authored-by: Darin Kotter <darin.kotter@gmail.com>

--- restricted_site_access.php
+++ restricted_site_access.php
@@ -1114,7 +1114,7 @@ class Restricted_Site_Access {
 		$script_path       = 'assets/js/build/settings.min.js';
 		$script_asset_path = plugin_dir_path( __FILE__ ) . 'assets/js/build/settings.min.asset.php';
 		$script_asset      = file_exists( $script_asset_path )
-			//phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable -- file_exists check ensures this is safe.
+			// phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable -- file_exists check ensures this is safe.
 			? require $script_asset_path
 			: array(
 				'dependencies' => array(),
@@ -1146,7 +1146,7 @@ class Restricted_Site_Access {
 		$script_path       = 'assets/js/build/admin.min.js';
 		$script_asset_path = plugin_dir_path( __FILE__ ) . 'assets/js/build/admin.min.asset.php';
 		$script_asset      = file_exists( $script_asset_path )
-			//phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable -- file_exists check ensures this is safe.
+			// phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable -- file_exists check ensures this is safe.
 			? require $script_asset_path
 			: array(
 				'dependencies' => array(),
